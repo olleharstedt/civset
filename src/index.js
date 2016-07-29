ecf3ab4f5e365aa6f3bc9ac735c05448e414f5e3
Olle Harstedt <olleharstedt@yahoo.com>
2016-07-30 01:26:16 +0200

    Some basic html

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 // @flow
 
-function foo(i: string) { return i; }
-console.log(foo('123'));
+class Agent {
+}
+
+$(document).ready(function() {
+
+  function initWorld() {
+    var world = new Array(10);
+    for (var i = 0; i < 100; i++) {
+      world[i] = new Array(100);
+    }
+    world[0][0] = {asd: 'qwe'};
+  }
+
+  initWorld();
+
+});
