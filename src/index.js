3b1bf97a4c6fdd9ed91e77100793cabde7ed5ed0
Olle Harstedt <olleharstedt@yahoo.com>
2016-07-30 19:33:07 +0200

    How to do static class variables?

--- src/index.js
+++ src/index.js
@@ -1,9 +1,55 @@
 // @flow
 
-class Agent {
+/**
+ * Position of an agent, building, resource, ...
+ */
+class Position {
+  x : number;
+  y : number;
+
+  constructor(x : number, y : number) {
+    this.x = x;
+    this.y = y;
+  }
 }
 
+/**
+ * 
+ */
+class Agent {
+  pos : Position;
+
+  /**
+   * @var {array}
+   */
+  static agents;
+
+  /**
+   * 
+   */
+  constructor() {
+    this.pos = new Position(0, 0);
+
+    // Add to agents array
+    Agent.agents.push(this);
+  }
+
+  /**
+   * @return {string} Table html
+   */
+  static updateAgentsTable() {
+    var table = $('#civset-agents-table');
+    for (var agent of Agent.agents) {
+      console.log(agent);
+    }
+    return `<div>Hej</div>`;
+  }
+}
+Agent.agents = []
+
 $(document).ready(function() {
+
+  console.log('here');
 
   function initWorld() {
     var world = new Array(10);
@@ -14,5 +60,9 @@ $(document).ready(function() {
   }
 
   initWorld();
+
+  var agent = new Agent();
+
+  Agent.updateAgentsTable();
 
 });
